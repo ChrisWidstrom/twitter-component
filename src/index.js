501a54bde67f5a7f00a29e7459e4f819140db236
Chris Widstrom <cwidstrom@me.com>
2023-02-17 19:34:28 -0600

    adds Avatar component.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import './index.css';
 
-ReactDOM.render(<h1>Hello!</h1>, document.querySelector('#root'));
+const Tweet = () => {
+    return <Avatar />
+}
+
+const Avatar = () => {
+    return (
+        <img src="https://cdn-icons-png.flaticon.com/512/147/147144.png" 
+        alt="Avatar"
+        />
+    )
+}
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
